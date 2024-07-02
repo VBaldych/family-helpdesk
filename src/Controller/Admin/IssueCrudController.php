c3603d95ff18e44322b2e5144c7980b0f4eef2d0
VBaldych <vovabaldych@gmail.com>
2024-07-03 00:52:39 +0300

    Create Issue CRUD controler

---
+++ src/Controller/Admin/IssueCrudController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Issue;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
+
+class IssueCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Issue::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Issues')
+            ->setEntityLabelInPlural('Issues')
+            ->setSearchFields(['author', 'text', 'email']);
+    }
+
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add(EntityFilter::new('user'));
+    }
+}
