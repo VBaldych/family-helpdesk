e1e1563bb70ff9fa279b087aad047d65bc67ab90
VBaldych <vovabaldych@gmail.com>
2024-07-10 11:12:40 +0300

    Update initial migration - create Admin user

--- migrations/Version20240710075353.php
+++ migrations/Version20240710075353.php
@@ -21,6 +21,7 @@ final class Version20240710075353 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE `admin` (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_IDENTIFIER_USERNAME (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql("INSERT INTO admin (id, username, roles, password) VALUES ('1', 'admin', '[\"ROLE_ADMIN\"]',  '\$2y\$13\$fI7wV3myOdZ6684QlmjiHOZuqkEV2uZV8y27KQTeBFUlkPRso4ldm')");
     }
 
     public function down(Schema $schema): void
