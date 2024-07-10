3ed3ce67ba4d93b387741f1521108886f2894ade
VBaldych <vovabaldych@gmail.com>
2024-07-10 08:09:44 +0300

    Add "Priority" field in Issue CRUD

--- src/Controller/Admin/IssueCrudController.php
+++ src/Controller/Admin/IssueCrudController.php
@@ -6,6 +6,9 @@ use App\Entity\Issue;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 
 class IssueCrudController extends AbstractCrudController
@@ -18,7 +21,7 @@ class IssueCrudController extends AbstractCrudController
     public function configureCrud(Crud $crud): Crud
     {
         return $crud
-            ->setEntityLabelInSingular('Issues')
+            ->setEntityLabelInSingular('Issue')
             ->setEntityLabelInPlural('Issues')
             ->setSearchFields(['author', 'text', 'email']);
     }
@@ -26,5 +29,16 @@ class IssueCrudController extends AbstractCrudController
     public function configureFilters(Filters $filters): Filters
     {
         return $filters->add(EntityFilter::new('user', 'Created by'));
+    }
+
+    public function configureFields(string $pageName): iterable {
+        yield TextField::new('title');
+        yield TextareaField::new('description');
+        yield ChoiceField::new('priority')->setChoices([
+            'Low' => 'priority_low',
+            'Medium' => 'priority_medium',
+            'High' => 'priority_high',
+            'Critical' => 'priority_critical',
+        ])->renderExpanded();
     }
 }
